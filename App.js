a3ee80b8f6a1e53085a51c15e45c3aa80421d267
Subham Kumar <subhamupadhya53100@gmail.com>
2025-10-23 14:45:48 +0530

    E-03 laying the foundation

--- App.js
+++ App.js
@@ -9,12 +9,11 @@
  *     <h1>I am h1 tag</h1>
  *     <h2>I am h2 tag</h2>
  *   </div>
- * </div>
+ * </div> 
  */
 
 import React from "react";
 import ReactDOM from "react-dom/client";
-
 
 const parent = React.createElement("div", { id: "parent" }, [
   React.createElement("div", { id: "child" }, [
@@ -27,5 +26,42 @@ const parent = React.createElement("div", { id: "parent" }, [
   ]),
 ]);
 
+// React.createElement => object => HTMLElement(render)
+const heading = React.createElement("h1", { id: "heading" }, "namaste react");
+console.log(heading);
+
+// jsx -> is not HTML is js.
+// (transpiled before it reaches the JS) - PARCEL - Babel 
+// Babel converted all the jsx code to react element and now the react understand the reat element 
+// JSX -> Babel transpiles it to React.createElement -> ReactElement-js Object -> HTMLElement(render)
+
+const jsxHeading = <h1 id="heading">Namaste react using jsx</h1>
+// console.log(jsxHeading);
+
+// root must be initialized before using
 const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(jsxHeading);
+
 root.render(parent);
+
+// REACT COMPONENT:- 2 types
+// class based component - OLD
+// Functional Component - NEW
+
+// React functional component:=> is a function that returns some pieces of jsx code 
+const HeadingComponent = () => {
+  return <h1 className="heading">Namaste React functional componenet</h1>
+};
+
+// component composition
+const HeadingComponent2 = () => {
+  return (
+    <div id="container">
+      {/* <Title /> */}
+      <h1 className="heading">Namaste React functional component</h1>
+    </div>
+  );
+};
+
+root.render(<HeadingComponent2 />);
