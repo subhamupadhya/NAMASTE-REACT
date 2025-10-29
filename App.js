8e3265e7acad5a684186c2a4669b9f0e611304a7
Subham Kumar <subhamupadhya53100@gmail.com>
2025-10-29 14:06:36 +0530

    E-04  Talk is cheap show me code.

--- App.js
+++ App.js
@@ -1,3 +1,264 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+/*
+* Header
+--> Logo
+--> Nav items
+* Body
+--> Search
+--> ResturantContainer
+  --> Resturant card
+    --->Img
+    --->Name, Rating, Cuisine, Delivery Time
+* Footer
+*/
+
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="logo-container">
+        <img
+          className="Logo"
+          src="https://www.logodesign.net/logo/smoking-burger-with-lettuce-3624ld.png"
+          alt="logo"
+        />
+      </div>
+      <div className="nav-items">
+        <ul>
+          <li>Home</li>
+          <li>About us</li>
+          <li>Contact us</li>
+          <li>Cart</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+const ResturantCard = (props) => {
+  const { resData } = props;
+
+  const {
+    name,
+    avgRating,
+    cuisines,
+    costForTwo,
+    deliveryTime,
+  } = resData?.data;
+
+  return (
+    <div className="resturant-card" style={{ backgroundColor: "#f0f0f0" }}>
+      <img
+        className="resturant-logo"
+        alt="resturant-logo"
+        src={resData.data.imageUrl}
+      />
+      <h3>{name}</h3>
+      <h4>{cuisines.join(", ")}</h4>
+      <h4>{avgRating} stars</h4>
+      <h4>₹{costForTwo / 100} FOR TWO</h4>
+      <h4>{deliveryTime} minutes</h4>
+    </div>
+  );
+};
+
+
+const restaurantList = [
+  {
+    data: {
+      name: "KFC",
+      cuisines: ["Burgers", "Fast Food"],
+      avgRating: "4.1",
+      costForTwo: 40000,
+      deliveryTime: 30,
+      imageUrl:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ2ntpLu0WTfHlFfZhcnJud0JLzCuBnIZKHmA&s",
+    },
+  },
+  {
+    data: {
+      name: "Domino’s Pizza",
+      cuisines: ["Italian", "Pizza", "Fast Food"],
+      avgRating: "4.3",
+      costForTwo: 50000,
+      deliveryTime: 25,
+      imageUrl:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRMZQOMZrKrNeoWiVHpzb4FQGMyiyvAxDrKpA&s",
+    },
+  },
+  {
+    data: {
+      name: "Subway",
+      cuisines: ["Healthy", "Salads", "Fast Food"],
+      avgRating: "4.0",
+      costForTwo: 35000,
+      deliveryTime: 20,
+      imageUrl:
+        "https://content.jdmagicbox.com/v2/comp/delhi/k9/011pxx11.xx11.190802211939.c5k9/catalogue/subway-delhi-fast-food-take-away-joints-consyduefz.jpg",
+    },
+  },
+  {
+    data: {
+      name: "McDonald's",
+      cuisines: ["Burgers", "Snacks", "Desserts"],
+      avgRating: "4.2",
+      costForTwo: 30000,
+      deliveryTime: 28,
+      imageUrl:
+        "https://www.shutterstock.com/image-photo/ayutthayathailand-mar-232022-mcdonalds-restaurant-260nw-2142997911.jpg",
+    },
+  },
+  {
+    data: {
+      name: "Burger King",
+      cuisines: ["Burgers", "American", "Fast Food"],
+      avgRating: "4.0",
+      costForTwo: 45000,
+      deliveryTime: 35,
+      imageUrl: "https://i.insider.com/5bd2234bdde867488579161f?width=700",
+    },
+  },
+  {
+    data: {
+      name: "Pizza Hut",
+      cuisines: ["Pizza", "Pasta"],
+      avgRating: "4.1",
+      costForTwo: 60000,
+      deliveryTime: 32,
+      imageUrl:
+        "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/13/98/25/56/pizza-hut.jpg?w=500&h=-1&s=1",
+    },
+  },
+  {
+    data: {
+      name: "Taco Bell",
+      cuisines: ["Mexican", "Wraps"],
+      avgRating: "4.2",
+      costForTwo: 35000,
+      deliveryTime: 29,
+      imageUrl:
+        "https://b.zmtcdn.com/data/pictures/6/19131716/24a2231f71c1ba0b8f57e53f54930ad3.jpg?fit=around|960:500&crop=960:500;*,*",
+    },
+  },
+  {
+    data: {
+      name: "Biryani Blues",
+      cuisines: ["Biryani", "Mughlai"],
+      avgRating: "4.4",
+      costForTwo: 55000,
+      deliveryTime: 27,
+      imageUrl:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTeOQVVjXByBxmLU_jtm_2OiezbpHS0lEafnA&s",
+    },
+  },
+  {
+    data: {
+      name: "Haldiram’s",
+      cuisines: ["Indian", "Snacks", "Sweets"],
+      avgRating: "4.3",
+      costForTwo: 25000,
+      deliveryTime: 22,
+      imageUrl:
+        "https://www.fnp.com/images/pr/usa/l/v20240528162931/haldiram-soan-papdi-250-gms_1.jpg",
+    },
+  },
+  {
+    data: {
+      name: "Barbeque Nation",
+      cuisines: ["North Indian", "Barbecue", "Desserts"],
+      avgRating: "4.5",
+      costForTwo: 90000,
+      deliveryTime: 40,
+      imageUrl:
+        "https://i0.wp.com/butterry.com/blog/wp-content/uploads/2023/06/barbeque-nation-buffet-price.webp?fit=1200%2C800&ssl=1",
+    },
+  },
+  {
+    data: {
+      name: "Wow! Momo",
+      cuisines: ["Tibetan", "Chinese"],
+      avgRating: "4.1",
+      costForTwo: 30000,
+      deliveryTime: 20,
+      imageUrl:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQj3caZ_gEF7X6okiFNTuC5LeVGeBqzJizN7w&s",
+    },
+  },
+  {
+    data: {
+      name: "Behrouz Biryani",
+      cuisines: ["Biryani", "Mughlai"],
+      avgRating: "4.3",
+      costForTwo: 60000,
+      deliveryTime: 33,
+      imageUrl: "https://rp-media.faasos.io/catalog/images/GYONIQMZX7G6.png",
+    },
+  },
+  {
+    data: {
+      name: "Chinese Wok",
+      cuisines: ["Chinese", "Asian"],
+      avgRating: "4.0",
+      costForTwo: 40000,
+      deliveryTime: 26,
+      imageUrl:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS917tWZ71puDP6W4WMt772m2yoiOuGtCsLzA&s",
+    },
+  },
+  {
+    data: {
+      name: "The Belgian Waffle Co.",
+      cuisines: ["Desserts", "Waffles"],
+      avgRating: "4.5",
+      costForTwo: 20000,
+      deliveryTime: 18,
+      imageUrl:
+        "https://b.zmtcdn.com/data/pictures/chains/3/18624503/f410a8bd4ccbe2d0febc882d0e2ef3f1.jpg?fit=around|960:500&crop=960:500;*,*",
+    },
+  },
+  {
+    data: {
+      name: "Starbucks",
+      cuisines: ["Coffee", "Beverages", "Snacks"],
+      avgRating: "4.6",
+      costForTwo: 70000,
+      deliveryTime: 24,
+      imageUrl:
+        "https://b.zmtcdn.com/data/pictures/chains/1/301011/13ed56679a2862840e4f7d4ca638aceb.jpg",
+    },
+  },
+];
+
+const Body = () => {
+  return (
+    <div className="body">
+      <div className="Search">Search</div>
+      <div
+        className="resturant-container"
+        style={{ display: "flex", flexWrap: "wrap" }}
+      >
+        {restaurantList.map((restaurant, index) => (
+          <ResturantCard key={index} resData={restaurant} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+const AppLayout = () => {
+  return (
+    <div className="app">
+      <Header />
+      <Body />
+    </div>
+  );
+};
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<AppLayout />);
+
+
 /**
  * 
  * <div id="parent">
@@ -12,56 +273,54 @@
  * </div> 
  */
 
-import React from "react";
-import ReactDOM from "react-dom/client";
 
-const parent = React.createElement("div", { id: "parent" }, [
-  React.createElement("div", { id: "child" }, [
-    React.createElement("h1", {}, "I am h1 tag"),
-    React.createElement("h2", {}, "I am h2 tag"),
-  ]),
-  React.createElement("div", { id: "child2" }, [
-    React.createElement("h1", {}, "I am h1 tag"),
-    React.createElement("h2", {}, "I am h2 tag"),
-  ]),
-]);
+
+// const parent = React.createElement("div", { id: "parent" }, [
+//   React.createElement("div", { id: "child" }, [
+//     React.createElement("h1", {}, "I am h1 tag"),
+//     React.createElement("h2", {}, "I am h2 tag"),
+//   ]),
+//   React.createElement("div", { id: "child2" }, [
+//     React.createElement("h1", {}, "I am h1 tag"),
+//     React.createElement("h2", {}, "I am h2 tag"),
+//   ]),
+// ]);
 
 // React.createElement => object => HTMLElement(render)
-const heading = React.createElement("h1", { id: "heading" }, "namaste react");
-console.log(heading);
+// const heading = React.createElement("h1", { id: "heading" }, "namaste react");
+// console.log(heading);
 
 // jsx -> is not HTML is js.
 // (transpiled before it reaches the JS) - PARCEL - Babel 
 // Babel converted all the jsx code to react element and now the react understand the reat element 
 // JSX -> Babel transpiles it to React.createElement -> ReactElement-js Object -> HTMLElement(render)
 
-const jsxHeading = <h1 id="heading">Namaste react using jsx</h1>
+// const jsxHeading = <h1 id="heading">Namaste react using jsx</h1>
 // console.log(jsxHeading);
 
 // root must be initialized before using
-const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(jsxHeading);
+// root.render(jsxHeading);
 
-root.render(parent);
+// root.render(parent);
 
 // REACT COMPONENT:- 2 types
 // class based component - OLD
 // Functional Component - NEW
 
 // React functional component:=> is a function that returns some pieces of jsx code 
-const HeadingComponent = () => {
-  return <h1 className="heading">Namaste React functional componenet</h1>
-};
+// const HeadingComponent = () => {
+//   return <h1 className="heading">Namaste React functional componenet</h1>
+// };
 
 // component composition
-const HeadingComponent2 = () => {
-  return (
-    <div id="container">
-      {/* <Title /> */}
-      <h1 className="heading">Namaste React functional component</h1>
-    </div>
-  );
-};
+// const HeadingComponent2 = () => {
+//   return (
+//     <div id="container">
+//       {/* <Title /> */}
+//       <h1 className="heading">Namaste React functional component</h1>
+//     </div>
+//   );
+// };
 
-root.render(<HeadingComponent2 />);
+
